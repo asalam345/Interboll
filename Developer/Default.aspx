3028e66ca82d91f116ea3fe6a391c4a17afac389
A. B. M. Abdus Salam <asalam345@gmail.com>
2025-03-15 17:31:43 +0600

    uploaded

---
+++ Developer/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Developer_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+     <fieldset style="width:auto;float:left;">
+        <legend>Domain</legend>
+         <p>
+             <asp:Label ID="lblSecretKey" runat="server" Text="Label"></asp:Label>
+         </p>
+         <p>
+         <asp:TextBox ID="TextBoxUrl" placeholder="Your Domain Name" style="width: 500px" runat="server"></asp:TextBox>
+        </p>
+        <p style="float:right; right:300px">
+            <asp:Button ID="btnDomainReg" runat="server" Text="OK" OnClick="btnDomainReg_Click" style="width:100px;"/>
+        </p>
+         
+    </fieldset>
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ Developer/Default.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Developer_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    public bool SignIn { set; get; }
+
+    protected void btnDomainReg_Click(object sender, EventArgs e)
+    {
+        DataSet ds = Articles.List_of_Domain();
+        DataTable dt = ds.Tables[0];
+        long id = 0;
+        if (dt.Rows.Count > 0)
+        {
+            id = Convert.ToInt64(dt.Rows[0][0]);
+        }
+        if(!String.IsNullOrEmpty(TextBoxUrl.Text))
+        {
+            SomeClasses sc = new SomeClasses();
+            string sChar = sc.GenerateCharecter();
+            DateTime c = DateTime.UtcNow;
+            sChar = c.Day.ToString() + c.Month + c.Year.ToString() + c.Hour.ToString() + c.Minute.ToString() + c.Second + c.Millisecond.ToString() + "_" + sChar;
+
+            try
+            {
+                Articles.OiiO_Domain_Insert(id + 1, TextBoxUrl.Text, sChar);
+                lblSecretKey.Text = sChar;
+            }
+            catch(Exception ex)
+            {
+                lblSecretKey.Text = ex.ToString();
+            }
+
+        }
+
+    }
+   
+
+   
+
+    //public ActionResult NotValid(string message)
+    //{
+    //    ViewBag.Message = message;
+    //    return View();
+    //}
+
+    //public ActionResult SignIn()
+    //{
+    //    return View();
+    //}
+
+   
+}
